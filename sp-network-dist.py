53268221e63c494f09468c05994f08b11a096bd8
miao3hu2 <miao3hu2@gmail.com>
2026-06-10 18:10:31 +0200

    adding free convolution

--- sp-network-dist.py
+++ sp-network-dist.py
@@ -1,4 +1,26 @@
 import numpy as np
+from numpy.typing import NDArray
+import sympy as sp
+
+class EmpricalEvalDist:
+    def __repr__(self):
+        pass
+
+    def support(self):
+        pass
+
+    def pdf(self):
+        pass
+
+    def Caychytransform(self):
+        pass
+
+    def Rtransform(self):
+        pass
+
+    def Stransform(self):
+        pass
+
 
 class MarchenkoPastur:
     def __init__(self, lam: float, sig: float = 1.0):
@@ -44,11 +66,47 @@ class MarchenkoPastur:
 
         return float(out[0]) if scalar else out
     
-    def Stieltjestransform(self):
-        pass
-    
-    def Rtransform(self):
+    def Caychytransform(self, z: NDArray[np.complex64] | complex) -> NDArray[np.complex64] | complex: 
         pass
 
-    def Stransform(self):
-        pass
+    def Rtransform(self, z: sp.Symbol) -> sp.Symbol:
+        return self.sig ** 2 / (1 - self.sig ** 2 * self.lam * z)
+    
+    # def Rtransform(self, z: NDArray[np.complex64] | complex) -> NDArray[np.complex64] | complex:
+    #     if not np.all(np.imag(z) > 0):
+    #         raise ValueError("Imaginary part must be positive")
+        
+    #     return self.sig ** 2 / (1 - self.sig ** 2 * self.lam * z)
+
+    def Stransform(self, z: sp.Symbol) -> sp.Symbol:
+        return 1 / (1 + self.lam * z) / self.sig ** 2
+
+    # def Stransform(self, z: NDArray[np.complex64] | complex) -> NDArray[np.complex64] | complex:
+    #     if not np.all(np.imag(z) > 0):
+    #         raise ValueError("Imaginary part must be positive")
+    
+    #     return 1 / (1 + self.lam * z) / self.sig ** 2
+
+def free_multiplicative_convolution(dist1: EmpricalEvalDist, dist2: EmpricalEvalDist) -> EmpricalEvalDist:
+    final_dist = EmpricalEvalDist
+    def finalStransform(z: sp.Symbol):
+        return dist1.Stransform(z) * dist2.Stransform(z)
+    
+    final_dist.Stransform = finalStransform
+
+    def finalRtransform(z: sp.Symbol):
+        w = sp.Symbol("w")
+        f_expr = z * finalStransform(z)
+        return sp.solve(sp.Eq(f_expr.subs(z, w), z), w)[0]
+
+    final_dist.Rtransform = finalRtransform
+
+    def finalCauchytransform(z: sp.Symbol):
+        w = sp.Symbol("w")
+        f_expr = (finalRtransform(z) + 1) / z
+        f_expr = (MarchenkoPastur(1).Rtransform(z) + 1) / z
+        return sp.solve(sp.Eq(f_expr.subs(z, w), z), w)[0]
+    
+    final_dist.Caychytransform = finalCauchytransform
+    
+    return final_dist
